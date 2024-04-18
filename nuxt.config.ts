69484ef3372a89b5b0322da5aafea6bf9fe7b74c
rapharrrr <125589553+rapharrrr@users.noreply.github.com>
2024-04-18 16:59:57 +0100

    grid.init onMounted WITH ssr turned down (production bug fix 3rd try)

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,7 +1,8 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   routeRules: {
-    '/lawsuits': {ssr: true}
+    '/lawsuits': {ssr: true},
+    '/dashboard': {ssr: false}
   },
   runtimeConfig: {
     public: {
--- pages/dashboard/index.vue
+++ pages/dashboard/index.vue
@@ -7,12 +7,12 @@ import { GridStack } from 'gridstack';
 
 // https://github.com/gridstack/gridstack.js/blob/master/doc/README.md#grid-options
 
-
+onMounted(() => {
             GridStack.init({ 
               float: true,
               cellHeight: "200px",
             });
-   
+          });
 </script>
 <template>
   
