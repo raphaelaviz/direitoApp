e6793b026657026222b2a8d2075456ceae021084
rapharrrr <125589553+rapharrrr@users.noreply.github.com>
2024-04-21 12:53:11 +0100

    missing colors bug fix (2nd try)

--- components/WidgetColorGrid.vue
+++ components/WidgetColorGrid.vue
@@ -5,24 +5,24 @@ const colors = [
   'pink-200',
   'yellow-200',
   'purple-200',
-  'blue-300', //
-  'pink-300', //
+  'blue-300', //missing in production
+  'pink-300', //missing in production
   'yellow-300',
   'purple-300',
-  'green-200', //
-  'red-300', //
-  'orange-300', //
+  'green-300', //missing in production
+  'red-300', //missing in production
+  'orange-300', //missing in production
   'red-400',
 ];
 
 </script>
 
 <template>
-  <div class="grid grid-cols-4 gap-2 ">
+  <div class="grid grid-cols-4 gap-2">
     <div
       v-for="color in colors" 
       :key="color" 
-      :class="`h-6 bg-${color} rounded-md hover:border-2 hover:border-slate-600 focus:bg-${color}`"
+      :class="`h-6 bg-${color} rounded-md hover:border-2 hover:scale-125 hover:border-slate-600 focus:bg-${color}`"
     />
   </div>
 </template>
